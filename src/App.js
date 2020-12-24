3f3c88682b27437ed96e7b6f1c6fa9150e537f8b
Kyler Mintah <kylermintah@gmail.com>
2020-12-24 16:02:45 -0500

    website init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import ParticlesBg from "particles-bg";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import './App.css';
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import ContactPage from './pages/ContactPage';
+import AboutPage from './pages/AboutPage';
+
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: 'Kyler Mintah',
+      headerLinks: [
+        { title: 'Home', path: '/' },
+        { title: 'About', path: '/about' },
+        { title: 'Contact', path: '/contact' },
+      ],
+      home: {
+        title: 'Into the Kyler-verse',
+        subtitle: 'What I spend my time doing',
+        text: '',
+      },
+      about: {
+        title: 'A bit about me',
+        subtitle: 'born in Virginia, USA, raised in Johannesburg, South Africa, recently spotted in Philadelphia, USA',
+        text: '',
+      },
+      contact: {
+        title: 'Let\'s Chat!',
+        subtitle: '',
+        text: '',
+      }
+    }
+  }
+
+  render() {
+    return (
+
+      <Router>
+
+        <Container className='p-2' fluid={true}>
+
+          <Navbar className='border-bottom' bg='transparent' expand='lg'>
+            <Navbar.Brand>
+              <a href='/'>Kyler Mintah</a>
+            </Navbar.Brand>
+
+            <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
+            <Navbar.Collapse id="navbar-toggle">
+              <Nav className='ml-auto'>
+                <Link className="nav-link" to="/">Home</Link>
+                <Link className='nav-link' to='/about'>About</Link>
+                <Link className='nav-link' to='/contact'>Contact</Link>
+              </Nav>
+            </Navbar.Collapse>
+
+          </Navbar>
+
+          <Route path="/" exact render={() => <HomePage title={this.state.home.title} subtitle={this.state.home.subtitle} text={this.state.home.text} />} />
+          <Route path="/about" exact render={() => <AboutPage title={this.state.about.title} />} />
+          <Route path="/contact" exact render={() => <ContactPage title={this.state.contact.title} />} />
+
+
+          <Footer>
+
+          </Footer>
+
+        </Container>
+      </Router>
+    );
+  }
+
 }
 
 export default App;
---
+++ src/components/CardInfo.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {useSpring, animated} from 'react-spring'; 
+
+function CardInfo(props) {
+
+    const style = useSpring({
+        opacity: 1, from:{opacity:0}
+    })
+
+    return (
+        <animated.div className="k-card-info" style={style}>
+            <h1 className='k-card-title'>{props.title}</h1>
+            <p className='k-card-subtitle'>{props.subtitle}</p>
+        </animated.div>
+    )
+}
+
+export default CardInfo;
